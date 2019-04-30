bf82664a6cac7afa0d29973e492ebdc7d12f39a5
Gabriel <gabriel@pelouze.net>
2019-04-30 23:55:46 +0200

    rm daily_reduce_and_plot.sh

--- daily_reduce_and_plot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-./reduce_boulders.py data/scraping/ data/reduced/
-./view_boulders.py data/reduced/latest_boulders.pkl data/plots/boulders.html
